33c4162aff9832593a23aeff97d03ce35a779b0b
andy159 <andy159@github.com>
2022-06-20 12:25:16 +0100

    Make it flash

--- ScreamingBoxPi/main.py
+++ ScreamingBoxPi/main.py
@@ -1,20 +1,21 @@
+from ScreamingBoxPi.flasher import Flasher
 from scream import Screamer
-import time
 from poller import Poller
-
 
 def main():
     screamer = Screamer()
     poller = Poller()
+    flasher = Flasher()
 
     while True:
         should_scream = poller.poll_api()
 
         if should_scream:
             screamer.scream()
+            flasher.flash()
         else:
             screamer.stop()
-        time.sleep(5)
+            flasher.stop()
 
 
 if __name__ == '__main__':
