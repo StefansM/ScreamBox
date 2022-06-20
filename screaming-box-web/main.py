2334750a4a51d9c037f5a880f26406571fe7dc5d
Simon Cowen <simoncowen88@users.noreply.github.com>
2022-06-20 13:08:15 +0100

    Add logger for 422 errors

--- screaming-box-web/main.py
+++ screaming-box-web/main.py
@@ -1,11 +1,22 @@
 from __future__ import annotations
 
-import fastapi
 import uvicorn
+from fastapi import FastAPI, Request, status
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+
 import webhook_event_handler
 from model import PollResult, WebhookResult, WebhookEvent
 
-app = fastapi.FastAPI()
+app = FastAPI()
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    exc_str = f'{exc}'.replace('\n', ' ').replace('   ', ' ')
+    print(f"{request}: {exc_str}")
+    content = {'status_code': 10422, 'message': exc_str, 'data': None}
+    return JSONResponse(content=content, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
 
 
 @app.get("/poll", response_model=PollResult)
